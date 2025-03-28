341d62a56892598afc03ac8dd742098b110901a0
Dy2561 <dylanyun2009@gmail.com>
2025-03-28 20:49:20 +0000

    Change 5

--- TowerSolver.java
+++ TowerSolver.java
@@ -7,6 +7,8 @@ private void solveRecursive(int n, int source, int destination, int auxiliary) {
     if (n == 1) {
         model.move(source, destination);
     } else {
-        
+        solveRecursive(n - 1, source, auxiliary, destination);
+        model.move(source, destination);
+        solveRecursive(n - 1, auxiliary, destination, source);
     }
 }
