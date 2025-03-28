d07b15d9321df147f0fe3a6145956f44fec2a037
Dy2561 <dylanyun2009@gmail.com>
2025-03-28 20:47:20 +0000

    Change 2

--- TowerModel.java
+++ TowerModel.java
@@ -7,4 +7,9 @@ public void move(int source, int destination) {
     int sourceDisk = sourceTower.peek();
     int destinationDisk = destinationTower.peek();
     
-   
+    if (sourceDisk != 0) {
+        if (destinationDisk == 0 || sourceDisk < destinationDisk) {
+            int disk = sourceTower.pop();
+            destinationTower.push(disk);
+            System.out.println("Moved disk " + disk + " from tower " + source + " to tower " + destination);
+        
