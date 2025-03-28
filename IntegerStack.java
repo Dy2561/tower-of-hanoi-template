bc52b367bd76b9c9a62235a0eb72ad1d5a6b0cf4
Dy2561 <dylanyun2009@gmail.com>
2025-03-28 21:04:45 +0000

    Change 10

--- IntegerStack.java
+++ IntegerStack.java
@@ -3,39 +3,47 @@ public class IntegerStack {
     private int[] stack;
     private int depth;
 
-    public IntegerStack(int depth) {
+    // Construct a stack of given depth
+    public IntegerStack(int depth)
+    {
         this.depth = 0;
         stack = new int[depth];
     }
 
-    public int peek() {
-        if (depth > 0) {
-            return stack[depth - 1];
+    // Get the value of the top element without removal
+    public int peek()
+    {
+        if (depth > 0)
+        {
+            return stack[depth -1];
         }
         return 0;
     }
 
-    public int pop() {
+    // Remove an element from the top of the stack.
+    public int pop()
+    {
         int result = 0;
         if (depth > 0) {
-            result = stack[depth - 1];
+            result = stack[depth-1];
             stack[depth - 1] = 0;
             depth--;
         }
         return result;
     }
 
-    public void push(int value) {
-        if ((depth >= 0) && (depth < stack.length)) {
+    // Insert an element at the top of the stack
+    public void push(int value)
+    {
+        if ((depth >= 0) && (depth < stack.length))
+        {
             stack[depth++] = value;
         }
     }
 
-    public int get(int index) {
+    // Debug function for printing
+    public int get(int index)
+    {
         return stack[index];
-    }
-
-    public int size() {
-        return depth;
     }
 }
