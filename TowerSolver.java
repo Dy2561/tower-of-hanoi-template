82f3f7cc46a9f0021d5b421ab966749cc917e6e4
Dy2561 <dylanyun2009@gmail.com>
2025-03-28 20:47:07 +0000

    Change1

--- TowerSolver.java
+++ TowerSolver.java
@@ -1,22 +1,14 @@
+public void solve(TowerModel model) {
+    this.model = model;
+    solveRecursive(model.height(), 0, 2, 1);
+}
 
-public class TowerSolver {
-    private TowerModel model;
-
-    public TowerSolver()
-    {
-        // Nothing to do here
+private void solveRecursive(int n, int source, int destination, int auxiliary) {
+    if (n == 1) {
+        model.move(source, destination);
+    } else {
+        solveRecursive(n - 1, source, auxiliary, destination);
+        model.move(source, destination);
+        solveRecursive(n - 1, auxiliary, destination, source);
     }
-
-    public void solve(TowerModel model)
-    {
-        this.model = model;
-        // Call the missing solve method (not this one)
-    }
-
-    // Create an overloaded solve(...) method
-    // This new method will be recursive (call itself)
-    //
-    // [ solve method here]
-    //
-
 }
